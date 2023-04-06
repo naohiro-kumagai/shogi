3b68089aa7fa6df3f3fef8c285381349f884a2de
kumagai <naohiro.kumagai@hotmail.com>
2023-04-06 23:42:25 +0900

    テスト追加

--- domain/Koma/NamaGoma/Fuhyou/index.spec.ts
+++ domain/Koma/NamaGoma/Fuhyou/index.spec.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest'
+import { Tokin } from '../../Narigoma/Tokin'
 import { Fuhyou } from '.'
 import { Masu } from '~~/domain/Taikyoku/ShogiBoard/Masu'
 import { CannotMoveError } from '~~/domain/DomainError'
@@ -8,5 +9,11 @@ describe('Fuhyou', () => {
     expect(new Fuhyou(new Masu(7, '七')).move(new Masu(7, '六'))).toStrictEqual(new Fuhyou(new Masu(7, '六')))
 
     expect(() => new Fuhyou(new Masu(7, '七')).move(new Masu(7, '五'))).toThrowError(new CannotMoveError('移動できません'))
+  })
+
+  it('条件を満たすとと金になれること', () => {
+    const fu = new Fuhyou(new Masu(7, '四'))
+    const fu2 = fu.move(new Masu(7, '三'))
+    expect(fu2.promote() instanceof Tokin).toBe(true)
   })
 })
